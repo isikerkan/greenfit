dde288fbf44f41992496c4d59a8cd50b7d408392
Melihtrk81 <melih.oezdemir@students.ffhs.ch>
2025-03-27 15:50:16 +0100

    AddingConsumption

--- App/src/main/java/ch/pa5/greenfit/model/SlotWithCalories.java
+++ App/src/main/java/ch/pa5/greenfit/model/SlotWithCalories.java
@@ -3,4 +3,4 @@ package ch.pa5.greenfit.model;
 import ch.pa5.greenfit.repository.entity.SlotEntity;
 import java.math.BigDecimal;
 
-public record SlotWithCalories(SlotEntity slot, BigDecimal calories) {}
+public record SlotWithCalories(SlotEntity slot, BigDecimal calories, List<ConsumptionEntity> consumption ) {}

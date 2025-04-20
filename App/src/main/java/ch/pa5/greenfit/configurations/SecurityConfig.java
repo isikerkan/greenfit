2d3a44f10eacea853e9f55519a37015e1f87c559
Melihtrk81 <melih.oezdemir@students.ffhs.ch>
2025-04-20 13:58:41 +0200

    deleting custom exception handling configuration

--- App/src/main/java/ch/pa5/greenfit/configurations/SecurityConfig.java
+++ App/src/main/java/ch/pa5/greenfit/configurations/SecurityConfig.java
@@ -17,10 +17,6 @@ public class SecurityConfig {
     http.cors(AbstractHttpConfigurer::disable)
         .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(requests -> requests.anyRequest().authenticated())
-//        .exceptionHandling(
-//            exception ->
-//                exception.authenticationEntryPoint(
-//                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
         .oauth2Login(Customizer.withDefaults());
     return http.build();
   }
